5f27f542832fb61447a9b2afb4836402b63623e9
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-28 12:58:18 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/SplashActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/SplashActivity.kt
@@ -1,21 +1,28 @@
  package com.example.ecommerceapp.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ecommerceapp.R
+import com.example.ecommerceapp.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+ class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+binding.startBtn.setOnClickListener {
+  startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
+}
+
     }
 }
