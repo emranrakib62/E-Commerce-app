022b6f8f2d3ce4cceaaa2225a11044dc9e2f05fc
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-29 12:10:46 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/Repository/MainRepository.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/Repository/MainRepository.kt
@@ -3,6 +3,8 @@ package com.example.ecommerceapp.Activity.Repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.ecommerceapp.Activity.Model.BrandModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
@@ -14,6 +16,18 @@ class MainRepository {
     fun loadBrands(){
         val ref=firebaseDatabase.getReference("Category")
         ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list=mutableListOf<BrandModel>()
+                for(childSnapshot in snapshot.children){
+                    childSnapshot.getValue(BrandModel::class.java)?.let {
+                        list.add(it)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
 
         })
     }
