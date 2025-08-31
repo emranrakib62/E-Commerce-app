fec89f82c670777084626cd9b59906d33a22a8dc
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-31 19:18:02 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/DashboardActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/DashboardActivity.kt
@@ -5,14 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.ecommerceapp.Activity.Adapter.BrandsAdapter
+import com.example.ecommerceapp.Activity.ViewModel.MainViewModel
 import com.example.ecommerceapp.R
+import com.example.ecommerceapp.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private val brandsAdapter = BrandsAdapter(mutableListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
