fc59ab6597b75e967d801ed76b902f6806e8436f
Emranul Hoque <emranrakib01862@gmail.com>
2025-08-31 19:37:36 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/DashboardActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/DashboardActivity.kt
@@ -1,15 +1,13 @@
 package com.example.ecommerceapp.Activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ecommerceapp.Activity.Adapter.BrandsAdapter
 import com.example.ecommerceapp.Activity.ViewModel.MainViewModel
-import com.example.ecommerceapp.R
 import com.example.ecommerceapp.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity() {
@@ -17,6 +15,7 @@ class DashboardActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
+
 
     private lateinit var binding: ActivityMainBinding
     private val brandsAdapter = BrandsAdapter(mutableListOf())
@@ -34,6 +33,17 @@ initBrands()
     }
     private fun initBrands(){
       binding.recyclerViewBrands.layoutManager= LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        binding.recyclerViewBrands.adapter=brandsAdapter
+        binding.progressBarBrands.visibility=View.VISIBLE
+
+
+        viewModel.brands.observe(this){
+           data ->
+         brandsAdapter.updateData(newData = data)
+            binding.progressBarBrands.visibility= View.GONE
+        }
+
     }
 
 
