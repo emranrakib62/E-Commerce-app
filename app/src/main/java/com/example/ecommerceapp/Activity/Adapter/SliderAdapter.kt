558959f40e9c4ba9b3ebf32c75d30412b159f884
Emranul Hoque <emranrakib01862@gmail.com>
2025-09-04 12:18:01 +0600

    updated sliderapdapter

--- app/src/main/java/com/example/ecommerceapp/Activity/Adapter/SliderAdapter.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/Adapter/SliderAdapter.kt
@@ -1,11 +1,17 @@
 package com.example.ecommerceapp.Activity.Adapter
 
+import android.R
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.request.RequestOptions
 import com.example.ecommerceapp.Activity.Model.SliderModel
+
 
 class SliderAdapter (
 
@@ -13,7 +19,16 @@ class SliderAdapter (
     private val viewPager2: ViewPager2
 ): RecyclerView.Adapter<SliderAdapter.SliderViewholder>(){
     class SliderViewholder (itemView: View): RecyclerView.ViewHolder(itemView){
-private val imageView: ImageView=itemView.findViewById(R.id.)
+private val imageView: ImageView=itemView.findViewById(R.id.imag)
+
+        fun setImage(sliderItems: SliderModel,context: Context){
+            val requestOption= RequestOptions().transform(CenterInside())
+            Glide.with(context)
+                .load(sliderItems.url)
+                .apply(requestOption)
+                .into(imageView)
+        }
+
     }
 
     override fun onCreateViewHolder(
@@ -27,9 +42,7 @@ private val imageView: ImageView=itemView.findViewById(R.id.)
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount()=sliderItems.size
 
 }
 
