e773953ad78fdfa61b3528611151c6f930912656
Emranul Hoque <emranrakib01862@gmail.com>
2025-09-06 11:19:48 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/ViewModel/MainViewModel.kt
@@ -3,13 +3,18 @@ package com.example.ecommerceapp.Activity.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.ecommerceapp.Activity.Model.BrandModel
+import com.example.ecommerceapp.Activity.Model.ItemModel
 import com.example.ecommerceapp.Activity.Model.SliderModel
 import com.example.ecommerceapp.Activity.Repository.MainRepository
 
 class MainViewModel : ViewModel() {
 private  val repository= MainRepository()
+
+    val popular: LiveData<MutableList<ItemModel>> = repository.popular
     val brands: LiveData<MutableList<BrandModel>> = repository.brands
     val banners: LiveData<List<SliderModel>> = repository.banners
+
     fun loadBrands() =repository.loadBrands()
     fun loadBanners() =repository.loadBanners()
+    fun loadPopular() =repository.loadPopular()
 }
