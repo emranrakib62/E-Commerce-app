44ea46384b6817c99a8f9428029356469bd6c111
Emranul Hoque <emranrakib01862@gmail.com>
2025-09-06 10:52:25 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/Model/ItemModel.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/Model/ItemModel.kt
@@ -1,3 +1,18 @@
 package com.example.ecommerceapp.Activity.Model
 
-data class ItemModel()
+import java.io.Serializable
+
+data class ItemModel(
+   var title: String="",
+    var description: String="",
+ var picurl: ArrayList<String> = ArrayList(),
+   var size: ArrayList<String> = ArrayList(),
+    var price : Double=0.0,
+   var oldprice : Double=0.0,
+   var rating: Double=0.0,
+   var numberIncart: Int=1,
+
+
+
+
+): Serializable
