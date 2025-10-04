0bea7b033f5b5018fcc69a709cb0407bf683aab1
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-05 01:04:21 +0600

    import helper

--- app/src/main/java/com/example/ecommerceapp/Activity/Activity/DetailActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/Activity/DetailActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerceapp.Activity.Model.ItemModel
 import com.example.ecommerceapp.R
+import com.example.ecommerceapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+private lateinit var binding: ActivityDetailBinding
+private lateinit var item: ItemModel
+private lateinit var managmentCart:
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
