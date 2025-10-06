1a0b665f6361b421ca2c5110c7e8b06b59df5cf3
Emranul Hoque <emranrakib01862@gmail.com>
2025-10-06 18:17:34 +0600

    updated

--- app/src/main/java/com/example/ecommerceapp/Activity/Adapter/ColorAdapter.kt
+++ app/src/main/java/com/example/ecommerceapp/Activity/Adapter/ColorAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.ecommerceapp.Activity.Adapter
 
-class ColorAdapter {
+import android.R
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ecommerceapp.databinding.ViewholderColorBinding
+
+class ColorAdapter (
+    private val items: ArrayList<String>
+): RecyclerView.Adapter<ColorAdapter.ViewHolder>(){
+
+private var selectedPosition=-1
+    private var lastSelectedPosition=-1
+    inner class ViewHolder (val binding: ViewholderColorBinding):
+    RecyclerView.ViewHolder(binding.root)
+
+
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ColorAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ColorAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
+
